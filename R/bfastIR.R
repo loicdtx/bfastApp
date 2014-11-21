f3634da29d25d818e6972413f3eacaab4cbbd943
Loïc Dutrieux <loic.dutrieux@gmail.com>
2014-11-21 15:00:50 +0100

    Wrote a simple version of bfastIR function

--- R/bfastIR.R
+++ R/bfastIR.R
@@ -1,3 +1,27 @@
-bfastIR <- function() {
-  
+#' Detect breakpoints in irregularly spaced time-series containing seasonal and trend components
+#' 
+#' @param x time-series object of class zoo
+#' @param order Numeric Harmonic order of the regression model
+#' @param formula See \link{breakpoints}
+#' @param breaks See \link{breakpoints}
+#' @param h See \link{breakpoints}
+#' @param stl Character See \link{bfastpp}
+#' 
+#' 
+#' @import strucchange
+#' @import zoo
+#' @import bfast
+#' 
+#' @export
+#' 
+
+
+bfastIR <- function(x, order=1, formula = response ~ trend + harmon, breaks = NULL, h = 0.15, stl = 'none') {
+  ts <- bfastts(data = x, dates = index(x), type = 'irregular')
+  pp <- bfastpp(ts, order = order, stl = stl)
+  breaks <- breakpoints(formula = formula, data = pp, breaks = breaks, h = h)
+  out <- list(zoo = x,
+              breaks = breaks)
+  class(out) <- 'bfastIR'
+  return(out)
 }
