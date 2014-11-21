ade5dbe63ce5ff57dc990b87441035f2f7523746
Loïc Dutrieux <loic.dutrieux@gmail.com>
2014-11-21 17:06:39 +0100

    Wrote a simple UI

--- ui.R
+++ ui.R
@@ -1,30 +1,37 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
+  titlePanel("bfastApp"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+      
+      fileInput(inputId = 'zooTs', label = 'rds file', multiple = FALSE),
+      
+      selectInput('formula',
+                  label = 'Formula',
+                  choices = list('trend',
+                                 'trend + harmon',
+                                 'harmon'),
+                  selected = 'trend + harmon'),
+      
+#       selectInput("id",
+#                   label = "Time Series Number", 
+#                   choices = as.list(as.character(seq(1,300))),
+#                   selected = '1'),
+      
+      numericInput("order", label = 'Harmonic order', value = 1),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+      numericInput("breaks", label = 'Number of breaks allowed', value = NULL),
+
+      sliderInput('h', label = 'minimal segment size', min = 0, max = 1, value = 0.15, step = 0.05)
+      
+      # character input for stl ??
+      
+        
+    ),
+    
+    mainPanel(plotOutput("bfastPlot"),
+              p('Work in progress'))
   )
 ))
