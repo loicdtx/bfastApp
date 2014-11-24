f54e23850e78de1a153aef4963cab882fbbc1b0f
Loïc <loic.dutrieux@gmail.com>
2014-11-24 11:03:19 +0100

    Added segments to the plotting function

--- R/ggplot.bfastIR.R
+++ R/ggplot.bfastIR.R
@@ -1,4 +1,4 @@
-ggplot.bfastIR <- function(x) {
+ggplot.bfastIR <- function(x, seg = TRUE) {
   
   ggdf <- x$df
   ggdf[,'breaks'] <- NA
@@ -6,7 +6,17 @@ ggplot.bfastIR <- function(x) {
   
   xIntercept <- ggdf$time[ggdf$breaks == 1]
   
-  ggplot(ggdf, aes(time, response)) +
+  gg <- ggplot(ggdf, aes(time, response)) +
     geom_line() +
     geom_vline(xintercept = xIntercept, color = 'red', linetype = 'dashed')
+  
+  if(seg) {
+    # Segments on time column
+    segments <- c(ggdf$time[c(1,x$breaks$breakpoints, nrow(ggdf))])
+    for(i in seq_along(segments[-1])) {
+      dfsub <- subset(ggdf, time <= segments[i + 1] & time >= segments[i])
+      gg <- gg + stat_smooth(method = "lm", data = dfsub, se = FALSE)
+    }    
+  }
+  gg
 }
